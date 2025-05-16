857e729dd7c2dc9a8b5d656709a4f7ee1ef8a09d
Pierre-Louis <pierrelouisdelcroix2001@gmail.com>
2025-05-16 22:41:28 +0200

    pretrained argument removed

--- neuroflux_analyzer/models.py
+++ neuroflux_analyzer/models.py
@@ -7,22 +7,22 @@ def get_model(model_name, num_classes):
     Supported models: 'efficientnet_b0', 'resnet50', 'densenet121', 'vgg16'
     """
     if model_name == 'efficientnet_b0':
-        model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT, pretrained=True)
+        model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
         in_features = model.classifier[3].in_features
         model.classifier[3] = nn.Linear(in_features, num_classes)
 
     elif model_name == 'resnet50':
-        model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT, pretrained=True)
+        model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
         in_features = model.fc.in_features
         model.fc = nn.Linear(in_features, num_classes)
 
     elif model_name == 'densenet121':
-        model = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT, pretrained=True)
+        model = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
         in_features = model.classifier.in_features
         model.classifier = nn.Linear(in_features, num_classes)
 
     elif model_name == 'vgg16':
-        model = models.vgg16(weights=models.VGG16_Weights.DEFAULT, pretrained=True)
+        model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
         in_features = model.classifier[6].in_features
         model.classifier[6] = nn.Linear(in_features, num_classes)
 
