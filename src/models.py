259db9b69c656e799ff22bd5776d408ae5414e00
Pierre-Louis <pierrelouisdelcroix2001@gmail.com>
2025-05-19 00:29:17 +0200

    resnet50 decay added

--- src/models.py
+++ src/models.py
@@ -7,28 +7,9 @@ import os
 def get_transfer_learning_model(model_name, num_classes, freeze_layers=True):
     """
     Loads a pre-trained model and replaces its classifier.
-    Supported models: 'resnet18', 'resnet50'
+    Supported models: 'resnet50'
     """
-    if model_name == 'resnet18':
-        model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-        if freeze_layers:
-            ct = 0
-            for child in model.children():
-                ct += 1
-                if ct < 7:  # Freeze layers before layer4 in ResNet
-                    for param in child.parameters():
-                        param.requires_grad = False
-
-            for param in model.fc.parameters():
-                param.requires_grad = True
-
-        in_features = model.fc.in_features
-        model.fc = nn.Sequential(
-            nn.Linear(in_features, 120),
-            nn.Linear(120, num_classes)
-        )
-
-    elif model_name == 'resnet50':
+    if model_name == 'resnet50':
         model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
         
         if freeze_layers:
@@ -43,12 +24,15 @@ def get_transfer_learning_model(model_name, num_classes, freeze_layers=True):
         # Replace the final layer with proper weight initialization
         in_features = model.fc.in_features
         model.fc = nn.Sequential(
-            nn.Dropout(p=0.5), 
-            nn.Linear(in_features, num_classes)
+            nn.Linear(in_features, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(p=0.6),
+            nn.Linear(512, num_classes)
         )
 
     else:
-        raise ValueError(f"Unsupported model: {model_name}")
+        raise ValueError(f"Unsupported model: {model_name}, please check the model_name in the config file")
 
     return model
 
