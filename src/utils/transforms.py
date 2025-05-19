21c1c0d45cf667ae4ce64c84b9fa46e3a37030e2
Pierre-Louis <pierrelouisdelcroix2001@gmail.com>
2025-05-19 19:37:03 +0200

    translate transformations removed

--- src/utils/transforms.py
+++ src/utils/transforms.py
@@ -5,7 +5,7 @@ def get_train_transforms(image_size=[224, 224]):
         transforms.Resize(image_size),
         transforms.Grayscale(num_output_channels=3),
         transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomAffine(degrees=(-10, 10), translate=(0.1, 0.1), scale=(0.9, 1.1)),
+        transforms.RandomAffine(degrees=(-10, 10), scale=(0.9, 1.1)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ])
